6b8af168817e44d880746a26693d6bc863ff1b5c
jacklavelle286 <lavellej286@gmail.com>
2025-03-28 18:37:06 +0000

    added infra pipeline

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -13,11 +13,13 @@ variable "mongo_password" {
 variable "region" {
   type = string
   description = "AWS region to deploy the resources"
+    default = "us-east-1"
   
 }
 
 variable "image_id" {
   type = string
   description = "AMI ID for the EC2 instance"
+  default = "ami-01a228f15911e06f6"
   
 }
