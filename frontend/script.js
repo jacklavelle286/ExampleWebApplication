79a1df5bab9296939ff34f1a21008a1c3d72b02a
jacklavelle286 <lavellej286@gmail.com>
2025-03-28 19:31:49 +0000

    edited dark mode on front page

--- frontend/script.js
+++ frontend/script.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
   const darkModeToggle = document.getElementById('dark-mode-toggle');
   const body = document.body;
  
-  // Check for saved user preference, if any, and apply it
   const userPrefersDark = localStorage.getItem('darkMode') === 'true';
   if (userPrefersDark) {
       body.classList.add('dark-mode');
@@ -11,7 +10,6 @@ document.addEventListener('DOMContentLoaded', function() {
   darkModeToggle.addEventListener('click', function() {
       body.classList.toggle('dark-mode');
  
-      // Save the user preference to local storage
       localStorage.setItem('darkMode', body.classList.contains('dark-mode'));
   });
  });
